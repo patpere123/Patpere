ed0f7bafdd2e03ee854ea338e133fbaafe1467ad
patpere123 <das.monojit@gmail.com>
2026-09-20 16:11:07 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -18,8 +18,8 @@ const FEATURED = {
   show: true,
   dishes: [
     { id:"fbf", name:"Fish Batter Fry", price:89, alone:true, minAlone:4, max:10 },
-    { id:"kschingri", name:"Kachu Shak Chingri", price:99, alone:true, minAlone:2, maxWithThali:2 },
-    { id:"dktl", name:"Doi Katla", price:90, alone:true, minAlone:2, maxWithThali:2 }
+    { id:"kschingri", name:"Kachu Shak Chingri", price:99, alone:true, minAlone:2, max:6 },
+    { id:"dktl", name:"Doi Katla", price:90, alone:true, minAlone:2, max:6 }
   ]
 };
 const ORDERS_OPEN = true;
